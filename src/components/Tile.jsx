6abe32282b277be8722ec3cb52c9bb140a0adc95
Maximus Teo <maximus.teo@uottawa.ca>
2025-05-25 19:03:56 -0400

    fixed flip animation

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Tile = ({ letter, status, isRevealed, delay = 0 }) => {
+
   const style = {
     animationDelay: isRevealed ? `${delay}ms` : '0ms'
   };
 
+  const [delayedStatus, setDelayedStatus] = useState('');
+
+  useEffect(() => {
+    if (isRevealed) {
+      const timeout = setTimeout(() => {
+        setDelayedStatus(status);
+      }, delay + 300);
+      return () => clearTimeout(timeout);
+    }
+  }, [isRevealed, status, delay]);
+
   return (
-    <div className="tile-container">
-      <div className={`tile ${status} ${isRevealed ? 'flip' : ''}`} style={style}>
+    <div className={`tile-container`}>
+      <div
+        className={`tile ${delayedStatus} ${isRevealed ? 'flip' : ''}`}
+        style={style}
+      >
         {letter}
       </div>
     </div>
